336476627c64b75f44a3ff870d32421621fb3d41
Felipe Elvas Barbosa <felipeelvas@gmail.com>
2024-09-13 21:00:39 -0300

    ng model ngForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextareaComponent } from './elements/textarea/textarea.component';
+import { InputTextComponent } from './elements/input-text/input-text.component';
+import { SelectComponent } from './elements/select/select.component';
+import { RadioButtonComponent } from './elements/radio-button/radio-button.component';
+import { CheckboxComponent } from './elements/checkbox/checkbox.component';
+import { FormComponent } from './elements/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextareaComponent,
+    InputTextComponent,
+    SelectComponent,
+    RadioButtonComponent,
+    CheckboxComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
